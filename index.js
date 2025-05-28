1963a68e3539b1d0d511ab32a0c4a70a998683a1
unknown <khawlagrissa@gmail.com>
2025-05-28 13:38:11 +0200

    Update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const os = require('os');
 require('dotenv').config();
 
 const app = express();
-//const port = process.env.PORT || 3000;
-const port = 80;
+const port = process.env.PORT || 3000;
+//const port = 80;
 
 // Middleware
 app.use(cors());
